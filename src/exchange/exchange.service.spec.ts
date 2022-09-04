115c7ea9269cc0cf456dbec3e29bd30b100b9cc3
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-09-04 18:37:56 -0300

    ensure convertAmount be throw getCurrency throw

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -51,5 +51,14 @@ describe('ExchangeService', () => {
         'BRL',
       );
     });
+
+    it('should be throw getCurrency throw', async () => {
+      (currenciesService.getCurrency as jest.Mock).mockRejectedValue(
+        new Error(),
+      );
+      await expect(
+        service.convertAmount({ from: 'INVALID', to: 'BRL', amount: 1 }),
+      ).rejects.toThrow();
+    });
   });
 });
