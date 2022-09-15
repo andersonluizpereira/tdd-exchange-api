cdb45721328f77c6583cabc47b4ea3f52a4dfb59
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-09-15 19:52:48 -0300

    ensure getCurrency throw if repository throw

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExchangeModule } from './exchange/exchange.module';
+import { CurrenciesModule } from './currencies/currencies.module';
 
 @Module({
-  imports: [ExchangeModule],
+  imports: [ExchangeModule, CurrenciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
